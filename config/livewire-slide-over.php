52c094ecb90765ce8e42b0389f463519c396a2af
batnieluyo <78667266+batnieluyo@users.noreply.github.com>
2024-06-27 11:56:04 -0600

    Update config

--- config/livewire-slide-over.php
+++ config/livewire-slide-over.php
@@ -6,6 +6,30 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Include CSS
+    |--------------------------------------------------------------------------
+    |
+    | The modal uses TailwindCSS, if you don't use TailwindCSS you will need
+    | to set this parameter to true. This includes the modern-normalize css.
+    |
+    */
+    'include_css' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Include JS
+    |--------------------------------------------------------------------------
+    |
+    | Livewire UI will inject the required Javascript in your blade template.
+    | If you want to bundle the required Javascript you can set this to false
+    | and add `require('vendor/wire-elements/modal/resources/js/modal');`
+    | to your script bundler like webpack.
+    |
+    */
+    'include_js' => false,
+    
+    /*
+    |--------------------------------------------------------------------------
     | Default Slide Over Position
     |--------------------------------------------------------------------------
     | Configure which way the slide-over will open
