70fe5268bd774fab6d313a14a4b9624849636ef0
Laurin Stapf <stapf_laurin@outlook.de>
2025-09-02 23:08:03 +0200

    Updated mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/slide-over.js', 'public/')
   .postCss('resources/css/slide-over.css', 'public/', [
-    require('tailwindcss'),
+    require('@tailwindcss/postcss'),
   ]);
