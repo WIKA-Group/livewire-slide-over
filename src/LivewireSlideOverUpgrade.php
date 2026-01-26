07bd4eff6fcf1ea62097491ac8d5ab4d8076e38e
Laurin Stapf <stapf_laurin@outlook.de>
2026-01-26 11:40:53 +0100

    Update command output

--- src/LivewireSlideOverUpgrade.php
+++ src/LivewireSlideOverUpgrade.php
@@ -7,7 +7,9 @@ use RecursiveIteratorIterator;
 
 class LivewireSlideOverUpgrade
 {
-    public static function run(string $projectRoot): void
+    private int $recordsProcessed = 0;
+
+    public static function handle(string $projectRoot): void
     {
         $files = new RecursiveIteratorIterator(
             new RecursiveDirectoryIterator($projectRoot . '/app')
@@ -18,16 +20,25 @@ class LivewireSlideOverUpgrade
                 continue;
             }
 
-            $content = file_get_contents($file->getPathname());
-
-            $content = str_replace(
-                'WireComponents\\LivewireSlideOvers\\',
-                'WikaGroup\\LivewireSlideOver\\',
-                $content);
-
-            file_put_contents($file->getPathname(), $content);
+            self::processFile($file);
         }
 
-        echo "Livewire slide over namespaces updated\n";
+        echo self::$recordsProcessed . " file(s) processed.\n\n";
+
+        echo "Livewire slide over namespaces updated.\n";
+    }
+
+    public function processFile($file): void
+    {
+        $content = file_get_contents($file->getPathname());
+
+        $content = str_replace(
+            'WireComponents\\LivewireSlideOvers\\',
+            'WikaGroup\\LivewireSlideOver\\',
+            $content);
+
+        file_put_contents($file->getPathname(), $content);
+
+        $this->recordsProcessed++;
     }
 }
